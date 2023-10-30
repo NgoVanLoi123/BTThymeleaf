2aff3ef8936fe6bdd673c40391da6b59f99a7882
NgoVanLoi123 <126578909+NgoVanLoi123@users.noreply.github.com>
2023-10-30 18:49:18 +0700

    sad

--- src/main/java/com/example/BTThymeleaf/model/request/PersonCreationRequest.java
+++ src/main/java/com/example/BTThymeleaf/model/request/PersonCreationRequest.java
@@ -3,6 +3,7 @@ import com.example.BTThymeleaf.statics.PersonCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.List;
@@ -16,6 +17,10 @@ public class PersonCreationRequest {
     private int id;
     private String name;
     private String address;
+//    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+
+
     private LocalDate birth ;
     private List<PersonCategory> categories;
     private String gender;
